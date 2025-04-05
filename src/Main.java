b1512ce7afb51be796220a90b6d2923164309072
Ashish <theashish752@gmail.com>
2025-04-05 15:50:05 +0530

    Change Date to IST

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import java.util.Date;
 
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+        ZonedDateTime currentDateTimeInIST = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        System.out.println("Hello, Ashish! Current date in IST: " + currentDateTimeInIST.format(formatter));
     }
 }
